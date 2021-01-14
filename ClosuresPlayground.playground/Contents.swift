40d56be77b76daa240650768ca39cef213f9541c
Wellesley Yu <byu@lcs.on.ca>
2021-01-14 11:57:29 -0500

    Testing out using closures as parameters.

--- ClosuresPlayground.playground/Contents.swift
+++ ClosuresPlayground.playground/Contents.swift
@@ -4,3 +4,15 @@ let driving = { (Place:String) in
     print("I'm driving to \(Place).")
 }
 driving("London")
+
+let drivingAgain = {
+    print("I'm driving in my car")
+}
+
+func travel(action: () ) {
+    print("I'm getting ready to go.")
+    action
+    print("I've arrived!")
+}
+
+travel(action: drivingAgain())
