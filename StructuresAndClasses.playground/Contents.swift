aea64218330315de9f0a971040d250174416c09c
Wellesley Yu <byu@lcs.on.ca>
2021-01-19 09:41:49 -0500

    Created a structures vs. classes playground and tested an example of structures value types.

---
+++ StructuresAndClasses.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+struct location {
+    var x : Int
+    var y : Int
+    
+}
+
+//September at LCS
+var home = location(x: 7, y: 10)
+var school = home
+
+//Where is home?
+home.x
+home.y
+
+//Where is school?
+school.x
+school.y
+
+//Chrismas vacation time
+home.x = 19
+home.y = 20
+
+home.x
+home.y
+
+school.x
+school.y
